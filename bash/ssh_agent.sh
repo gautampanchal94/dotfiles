e53d9c2ff9af6bc73279a172895d9e1c21135af7
Gautam Panchal <gautampanchal01@gmail.com>
2022-11-22 12:38:31 +0530

    shell script for ssh-agent added

---
+++ bash/ssh_agent.sh
@@ -0,0 +1,24 @@
+# set up ssh-agent
+SSH_ENV="$HOME/.ssh/enviroment"
+SSH_KEY="$HOME/.ssh/path/ssh-key"
+
+# for initializing ssh agent
+function start_agent {
+    echo "Initializing new SSH agent..."
+    touch $SSH_ENV
+    chmod 600 "${SSH_ENV}"
+    /usr/bin/ssh-agent | sed 's/^echo/#echo/' >> "${SSH_ENV}"
+    . "${SSH_ENV}" > /dev/null
+    /usr/bin/ssh-add
+    ssh-add $SSH_KEY
+}
+
+# source SSH settings if applicable
+if [ -f "${SSH_ENV}" ]; then
+    . "${SSH_ENV}" > /dev/null
+    kill -0 $SSH_AGENT_PID 2>/dev/null || {
+        start_agent
+    }
+else
+    start_agent
+fi
